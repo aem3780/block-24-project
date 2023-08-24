b96271cd205b73b80d6add93130e839d85ec447b
Allie Maus <aem3780@rit.edu>
2023-08-24 13:17:17 -0400

    exercise using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-
-import './App.css'
+import "./App.css";
+import { puppyList } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((puppy) => puppy.id === featPupId);
+  console.log(featuredPup);
 
+  console.log(puppies);
   return (
     <>
       <div>
-
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.age}</h2>
+            <ul>
+              <li>
+                Age:
+                {featuredPup.age}
+              </li>
+              <li>
+                Email:
+                {featuredPup.email}
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
